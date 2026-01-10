d73f07490fb7a39f1a98abdd62c982620631edc9
Moazzem-Bhuiyan <tiyonbhuiyan.201@gmail.com>
2026-01-10 13:33:39 +0600

    update next virsion

--- src/app/(Auth)/login/page.js
+++ src/app/(Auth)/login/page.js
@@ -2,7 +2,7 @@ import LoginForm from "./_components/LoginForm";
 
 export const metadata = {
   title: "Login",
-  description: "Sign in to admin dashboard of Before After Story",
+  description: "Sign in to admin dashboard of Yalla Coupons",
 };
 export default function LoginPage() {
   return (
